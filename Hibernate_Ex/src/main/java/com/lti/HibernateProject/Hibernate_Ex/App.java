eb3c9f723c7a8e3e0f71c30dd54a0f2be7cb0d20
mukul <mukulbabel@gmail.com>
2019-11-06 15:08:42 +0530

    "New Project"

---
+++ Hibernate_Ex/src/main/java/com/lti/HibernateProject/Hibernate_Ex/App.java
@@ -0,0 +1,13 @@
+package com.lti.HibernateProject.Hibernate_Ex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
